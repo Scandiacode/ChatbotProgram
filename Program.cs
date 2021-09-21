34ae09d7541a752d1584814c9068b22da8127af0
Logan Harris <logan.harris41@yahoo.com>
2021-09-21 00:22:56 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string starterInput;
+            string mainInput;
+            bool byeAnswer = false;
+
+            string doubleSay = "";
+
+            while (!byeAnswer)
+            {
+                Console.WriteLine("What do you want to say to the bot?\n");
+                
+                starterInput = Console.ReadLine();
+                mainInput = starterInput.ToLower();
+
+                if (!mainInput.Equals(doubleSay))
+                {
+                    //Console.WriteLine(mainInput);
+
+
+                    if (mainInput.Equals("hello"))
+                    {
+                        Console.WriteLine("Hi good to see you!\n");
+                        
+                    }
+                    else if (mainInput.Equals("sup"))
+                    {
+                        Console.WriteLine("I am good.\n");
+                        
+                    }
+                    else if (mainInput.Equals("hello there"))
+                    {
+                        Console.WriteLine("General Kenobi!\n");
+                       
+                    }
+                    else
+                    {
+                        byeAnswer = true;
+                    }
+
+
+                    doubleSay = mainInput;
+
+
+                }
+                else
+                {
+                    Console.WriteLine("I'm sorry but you have already said that.\n");
+                    
+                }
+            }
+            
+
+            Console.WriteLine("Good bye!");
+            
+        }
+    }
+}
